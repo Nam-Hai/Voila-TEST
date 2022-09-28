ab12bb35c8f6d1bf31704a6237e48ab79400febd
BlueTompon <lucastran00@gmail.com>
2022-09-28 16:48:13 +0200

    no loader

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
-import fakeLoaderAnimation from './annimation/fakeLoaderAnimation'
 import InitAnimation from './annimation/InitAnimation'
 import TitleInitAnimation from './annimation/titleInitAnimation'
 import './style.scss'
 import { N } from './utils/namhai'
 async function app() {
     N.TopReload()
-    await new Promise(s => {
-        new fakeLoaderAnimation(s)
-    })
+
 
     new TitleInitAnimation()
 
